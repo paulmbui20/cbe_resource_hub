09da79b943cc745b2b7ebf894860c2add6e4143b
paulmbui <paulmbui20@gmail.com>
2026-04-26 16:28:32 +0300

    added tests classes in the test_admin_views.py file to the __all__ list in the __init__.py file

--- accounts/tests/__init__.py
+++ accounts/tests/__init__.py
@@ -1,6 +1,12 @@
+from .test_admin_views import (
+    IsAdminMixinTests, AdminUserListViewTests, AdminUserCreateViewTests, AdminUserUpdateViewTests,
+    AdminUserDeleteViewTests, AdminUserBulkToggleViewTests
+)
 from .test_forms import ProfileFormTestCase
 from .test_models import TestUserCreation
-from .test_views import AccountsProfileViewTests, AccountsBecomeVendorViewTests, AccountsDashboardViewTests
+from .test_views import (
+    AccountsProfileViewTests, AccountsBecomeVendorViewTests, AccountsDashboardViewTests
+)
 
 __all__ = [
     "TestUserCreation",
@@ -8,4 +14,10 @@ __all__ = [
     "AccountsProfileViewTests",
     "AccountsBecomeVendorViewTests",
     "AccountsDashboardViewTests",
+    "IsAdminMixinTests",
+    "AdminUserListViewTests",
+    "AdminUserCreateViewTests",
+    "AdminUserUpdateViewTests",
+    "AdminUserDeleteViewTests",
+    "AdminUserBulkToggleViewTests",
 ]
